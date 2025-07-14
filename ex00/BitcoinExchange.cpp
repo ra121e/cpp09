f472158cce811ba72235ace3610f9614ca77e8f2
root <sushi.honda@gmail.com>
2025-07-14 08:52:48 +0800

    ex00) feat: constructors
      considered that copy constructor & assignement operator are no need, because exchange should be only one.
      but OCF...
    
      In constructor
      - setting ratemap is called in the body of constructor
      - In default constructor, no need initialization because std::map does
      - other than same style to initialization

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 17:10:39 by athonda           #+#    #+#             */
-/*   Updated: 2025/07/12 18:55:49 by athonda          ###   ########.fr       */
+/*   Updated: 2025/07/14 08:50:46 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange()
+{
+	setRate("data.csv");
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &other):
+	_ratemap(other._ratemap);
 {}
+
+BitcoinExchange	&BitcoinExchange::operator=(BitcoinExchange const &other)
+{
+	if (this != &other)
+	{
+		this->_ratemap = other._ratemap;
+	}
+	return (*this);
+}
 
 BitcoinExchange::~BitcoinExchange()
 {}
@@ -32,6 +47,12 @@ void	BitcoinExchange::setRate(std::string const &filename)
 	std::getline(ifs, line);
 
 	std::cout << line << std::endl;
+	while (std::getline(ifd, line))
+	{
+		std::string	date;
+		std::string	rate;
 
+		line.
+	}
 
 }

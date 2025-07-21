c97ea4dd919e0cc7ede4a81b5717310b69dd6760
root <sushi.honda@gmail.com>
2025-07-21 15:51:23 +0800

    ex00) feat: input file as command line argument

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:37:13 by athonda           #+#    #+#             */
-/*   Updated: 2025/07/21 15:41:45 by athonda          ###   ########.fr       */
+/*   Updated: 2025/07/21 15:50:23 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 
 int	main(int ac, char **av)
 {
-	(void)av;
 	if (ac != 2)
 	{
-		std::cout << "Usage: ./btc filename.csv" << std::endl;
+		std::cout << "Usage: ./btc input.txt" << std::endl;
+		return (1);
 	}
+	std::string	inputfile(av[1]);
 	BitcoinExchange	a;
 //	a.setRate("data.csv");
-	a.inputFile("input.txt");
+	a.inputFile(inputfile);
 	return (0);
 }

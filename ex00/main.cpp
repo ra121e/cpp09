59c4b30368641b7c7544be18f949a6a097dc345f
root <sushi.honda@gmail.com>
2025-11-08 09:17:32 +0800

    ex00) style: variable correction
      in main process, command line argument is used as the file name.
      so the std::string variable is "file name", not "file"
    
      Moreover, previous "inputfile" is used as method name of string manipulation...

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:37:13 by athonda           #+#    #+#             */
-/*   Updated: 2025/11/08 09:12:15 by athonda          ###   ########.fr       */
+/*   Updated: 2025/11/08 09:17:11 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	main(int ac, char **av)
 		std::cout << "Usage: ./btc input.txt" << std::endl;
 		return (1);
 	}
-	std::string	inputfile(av[1]);
+	std::string	input_file_name(av[1]);
 	BitcoinExchange	bit_coin_exchange;
 	if (!bit_coin_exchange.setRate("data.csv"))
 		return (1);
-	bit_coin_exchange.inputFile(inputfile);
+	bit_coin_exchange.inputFile(input_file_name);
 	return (0);
 }
